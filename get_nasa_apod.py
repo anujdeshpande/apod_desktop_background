f3f8d03c238bdbff6b59d319948d9b3f67532849
Anuj Deshpande <anujdeshpande92@gmail.com>
2013-12-29 00:56:03 +0530

    add sqlite and killall command to python file

--- get_nasa_apod.py
+++ get_nasa_apod.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup
-import urllib2, re, datetime
+import urllib2, re, datetime, os
 
 url='http://apod.nasa.gov/apod/'
 response=urllib2.urlopen(url)
@@ -14,4 +14,5 @@ store.write ( response.read())
 store.close()
 response.close()
 
-
+os.system("sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db \"update data set value = '/Users/anuj/code/nasa_desktop_background/"+str(datetime.date.today())+".jpg"+"'\";")
+os.system("killall Dock")
